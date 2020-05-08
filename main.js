19b2c85c76e7d7f13d55bef5fb77ef28a9c53ab6
janne-remes <janneremes@outlook.com>
2020-05-08 08:54:47 +0300

    prototyypi-version jatkamista

--- main.js
+++ main.js
@@ -1,29 +1,4 @@
 var askedQuestion = document.getElementById("questionline").textContent;
-var usersAnswer; //= document.getElementById("answerBlockOne").textContent;
-
-/*if (document.getElementById("answerOneChoosed").onclick = true){
-    usersAnswer = document.getElementById("answerBlockOne").textContent;
-}
-
-if (document.getElementById("answerTwoChoosed").onclick = true){
-    usersAnswer = document.getElementById("answerBlockTwo").textContent;
-}
-
-if (document.getElementById("answerThreeChoosed").onclick = true){
-    usersAnswer = document.getElementById("answerBlockThree").textContent;
-}
-
-if (selectAnswerOne()){
-    usersAnswer = document.getElementById("answerBlockOne").textContent;
-}
-
-else if(selectAnswerTwo()){
-    usersAnswer = document.getElementById("answerBlockTwo").textContent;
-}
-
-else if(selectAnswerThree()){
-    usersAnswer = document.getElementById("answerBlockThree").textContent;
-}*/
 
 function onStart(){
     document.getElementById("questionsBlock").style.visibility = "hidden";
@@ -39,32 +14,29 @@ function startQuestionnaire(){
 function selectAnswerOne(){
     usersAnswer = document.getElementById("answerBlockOne").textContent;
 
-    document.getElementById("questionsBlock").remove();
+    document.getElementById("questionsBlock").style.visibility = "hidden";
     document.getElementById("endSummary").style.visibility = "visible";
 
-    return usersAnswer;
+    document.getElementById("resultAnalysis").innerHTML = "Kysymykseen <b>" + askedQuestion + "</b> sinä vastasit <b> " + usersAnswer + "</b>";
 }
 
 function selectAnswerTwo(){
     usersAnswer = document.getElementById("answerBlockTwo").textContent;
 
-    document.getElementById("questionsBlock").remove();
+    document.getElementById("questionsBlock").style.visibility = "hidden";
     document.getElementById("endSummary").style.visibility = "visible";
 
-    return usersAnswer;
+    document.getElementById("resultAnalysis").innerHTML = "Kysymykseen <b>" + askedQuestion + "</b> sinä vastasit <b> " + usersAnswer + "</b>";
 }
 
 function selectAnswerThree(){
     usersAnswer = document.getElementById("answerBlockThree").textContent;
 
-    document.getElementById("questionsBlock").remove();
+    document.getElementById("questionsBlock").style.visibility = "hidden";
     document.getElementById("endSummary").style.visibility = "visible";
 
-    return usersAnswer;
+    document.getElementById("resultAnalysis").innerHTML = "Kysymykseen <b>" + askedQuestion + "</b> sinä vastasit <b> " + usersAnswer + "</b>";
 }
-
-
-document.getElementById("resultAnalysis").innerHTML = "Kysymykseen <b>" + askedQuestion + "</b> sinä vastasit <b> " + usersAnswer + "</b>";
 
 console.log(usersAnswer);
 console.log(askedQuestion);
